2c9bd6c402a44a9f8c37fcd0c17ce9199079b69c
Emanuel Evans <me@eevans.co>
2021-10-23 15:15:52 -0700

    Hook up a bunch of glue code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 mod utils;
+mod webrtc_rpc;
 
-use std::time::Duration;
+use futures::prelude::*;
 use wasm_bindgen::prelude::*;
-use web_sys::{console, Element};
+use wasm_bindgen::JsCast;
+use web_sys::{Document, Event, HtmlButtonElement, HtmlElement, HtmlInputElement, Window};
+use webrtc_rpc::introduce;
+use wasm_bindgen_futures::spawn_local;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -10,29 +14,95 @@ use web_sys::{console, Element};
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-struct Raft {
-    elem: Element,
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-impl Raft {
-    pub async fn run(&mut self) {
-        console::log_1(&"YO!".into());
-        self.elem.set_inner_html("<h1>YOOOOO</h1>");
-        utils::sleep(Duration::from_secs(3)).await.unwrap();
-        self.elem.set_inner_html("<h1>HAHAHA</h1>");
-    }
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
+// struct Raft {
+//     elem: Element,
+// }
+
+// impl Raft {
+//     pub async fn run(&mut self) {
+//         console::log_1(&"YO!".into());
+//         self.elem.set_inner_html("<h1>YOOOOO</h1>");
+//         utils::sleep(Duration::from_secs(3)).await.unwrap();
+//         self.elem.set_inner_html("<h1>HAHAHA</h1>");
+//     }
+// }
 
 #[wasm_bindgen(start)]
 pub async fn start() {
     utils::set_panic_hook();
-    let document = web_sys::window()
-        .expect("no global window")
-        .document()
-        .expect("window should have document");
-    let elem = document
-        .get_element_by_id("content")
-        .expect("No content found");
-    let mut raft = Raft { elem };
-    raft.run().await;
+    let hostname = get_window().location().hostname().expect("no hostname");
+    let document = get_document();
+
+    let start_button_elem = document
+        .get_element_by_id("start-button")
+        .expect("No start button found");
+    let start_button = start_button_elem
+        .dyn_ref::<HtmlButtonElement>()
+        .expect("#start-button should be a button element");
+    let hn_start = hostname.clone();
+    let start = Closure::wrap(Box::new(move |ev: Event| {
+        let hn = hn_start.clone();
+        ev.prevent_default();
+        hide_start_form();
+        spawn_local(async move {
+            introduce(hn.clone(), get_session_key(), true).await;
+        });
+    }) as Box<dyn FnMut(Event)>);
+    start_button.set_onclick(Some(start.as_ref().unchecked_ref()));
+
+    let join_button_elem = document
+        .get_element_by_id("join-button")
+        .expect("No join button found");
+    let join_button = join_button_elem
+        .dyn_ref::<HtmlButtonElement>()
+        .expect("#join-button should be a button element");
+    let hn_join = hostname.clone();
+    let join = Closure::wrap(Box::new(move |ev: Event| {
+        let hn = hn_join.clone();
+        ev.prevent_default();
+        hide_start_form();
+        spawn_local(async move {
+            introduce(hn.clone(), get_session_key(), false).await;
+        });
+    }) as Box<dyn FnMut(Event)>);
+    join_button.set_onclick(Some(join.as_ref().unchecked_ref()));
+
+    let forever = future::pending();
+    let () = forever.await;
+    unreachable!();
+}
+
+fn get_session_key() -> String {
+    let elem = get_document()
+        .get_element_by_id("session-key")
+        .expect("session-key input not found");
+    elem.dyn_ref::<HtmlInputElement>()
+        .expect("session-key should be an input element")
+        .value()
+}
+
+fn hide_start_form() {
+    let elem = get_document()
+        .get_element_by_id("start-form")
+        .expect("start-form not found");
+    elem.dyn_ref::<HtmlElement>()
+        .expect("start-form should be an HTML element")
+        .set_hidden(true);
+}
+
+fn get_window() -> Window {
+    web_sys::window().expect("no global window")
+}
+
+fn get_document() -> Document {
+    get_window().document().expect("no global document exists")
 }
---
+++ src/webrtc_rpc/mod.rs
@@ -0,0 +1,18 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+pub async fn introduce(id: String, session_id: String, initiate: bool) {
+    console_log!("INTRODUCE");
+    console_log!("ID: {:#?}", id);
+    console_log!("SESSION_ID: {:#?}", session_id);
+    console_log!("INITIATE: {:#?}", initiate);
+}
