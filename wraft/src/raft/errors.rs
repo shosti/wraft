45b2f3a6df87dc033e1d9a86c2fcd5b38d5f2af5
Emanuel Evans <me@eevans.co>
2021-10-29 15:41:06 -0700

    Clean things up

--- wraft/src/raft/errors.rs
+++ wraft/src/raft/errors.rs
@@ -1,15 +1,38 @@
 use crate::webrtc_rpc::transport;
-use crate::raft::persistence::DBError;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::Event;
 
 #[derive(Debug)]
 pub enum Error {
-    TransportError(transport::Error),
+    Js(String),
+    Transport(transport::Error),
     NotEnoughPeers(),
-    DatabaseError(DBError),
+    Persistence(serde_json::Error),
 }
 
-impl From<DBError> for Error {
-    fn from(err: DBError) -> Self {
-        Error::DatabaseError(err)
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Error::Persistence(err)
+    }
+}
+
+impl From<JsValue> for Error {
+    fn from(err: JsValue) -> Self {
+        let msg = match err.as_string() {
+            Some(e) => e,
+            None => {
+                if let Some(ev) = err.dyn_ref::<Event>() {
+                    format!(
+                        "error on event with type {} and target {:?}",
+                        ev.type_(),
+                        ev.target()
+                    )
+                } else {
+                    format!("JS error: {:?}", err)
+                }
+            }
+        };
+        Error::Js(msg)
     }
 }
--- wraft/src/raft/persistence.rs
+++ wraft/src/raft/persistence.rs
@@ -1,10 +1,7 @@
 use crate::raft::errors::Error;
 use serde::{Deserialize, Serialize};
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{DomException, Event};
 
-type LogPosition = u64;
+pub type LogPosition = u64;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LogEntry {
@@ -26,14 +23,16 @@ pub struct PersistentState {
 
 impl PersistentState {
     pub async fn initialize(session_id: &str) -> Result<Self, Error> {
-        Ok(Self { session_id: session_id.to_string() })
+        Ok(Self {
+            session_id: session_id.to_string(),
+        })
     }
 
     pub async fn append(&self, entry: LogEntry) -> Result<(), Error> {
         let key = format!("log-{}-{}", self.session_id, entry.position);
         let window = web_sys::window().expect("no global window");
         let storage = window.local_storage().unwrap().unwrap();
-        let data = serde_json::to_string(&entry).unwrap();
+        let data = serde_json::to_string(&entry)?;
         storage.set_item(&key, &data).unwrap();
         Ok(())
     }
@@ -43,46 +42,7 @@ impl PersistentState {
         let window = web_sys::window().expect("no global window");
         let storage = window.local_storage().unwrap().unwrap();
         let data = storage.get_item(&key).unwrap().unwrap();
-        let entry: LogEntry = serde_json::from_str(&data).unwrap();
+        let entry: LogEntry = serde_json::from_str(&data)?;
         Ok(entry)
-    }
-}
-
-#[derive(Debug)]
-pub enum DBError {
-    Js(String),
-    Serialization(Box<bincode::ErrorKind>),
-}
-
-impl From<JsValue> for DBError {
-    fn from(err: JsValue) -> Self {
-        let msg = match err.as_string() {
-            Some(e) => e,
-            None => {
-                if let Some(ev) = err.dyn_ref::<Event>() {
-                    format!(
-                        "error on event with type {} and target {:?}",
-                        ev.type_(),
-                        ev.target()
-                    )
-                } else {
-                    format!("database error: {:?}", err)
-                }
-            }
-        };
-        DBError::Js(msg)
-    }
-}
-
-impl From<DomException> for DBError {
-    fn from(err: DomException) -> Self {
-        let msg = format!("{}: {}", err.name(), err.message());
-        DBError::Js(msg)
-    }
-}
-
-impl From<Box<bincode::ErrorKind>> for DBError {
-    fn from(err: Box<bincode::ErrorKind>) -> Self {
-        DBError::Serialization(err)
     }
 }
